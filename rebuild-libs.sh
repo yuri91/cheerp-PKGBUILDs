07b3ed881519e299eaba2aa4f1eacead75d01536
Yuri Iozzelli <y.iozzelli@gmail.com>
2021-04-13 21:21:11 +0200

    update

---
+++ rebuild-libs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# print all statements
+set -x
+# exit on first error
+set -e
+# exit if variable not defined
+set -u
+
+function build_from_scratch {
+	pushd $1
+	rm -rf src/ pkg/ *.xz
+	makepkg -si
+	popd
+}
+
+build_from_scratch cheerp-newlib
+build_from_scratch cheerp-libcxx
+build_from_scratch cheerp-libcxxabi
+build_from_scratch cheerp-libs
